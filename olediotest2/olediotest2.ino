0c5f268e0a57feb4d97cd3cb863e80e3d186b35b
Rusith Umanga <rusith.umanga@gmail.com>
2024-08-03 04:27:46 +0530

    Interface Rework

--- olediotest2/olediotest2.ino
+++ olediotest2/olediotest2.ino
@@ -3,8 +3,8 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define BTN D5  //Main Button
-#define RST D6  //Reset Button
+#define BTN1 D5  //Main Button
+#define BTN2 D6  //Reset Button
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
@@ -28,160 +28,338 @@ unsigned long disprMillis = 0;  //Display Refresh Timer
 
 bool meter = 0;     //PZEM Connection
 bool wifistat = 0;  //Wifi Connection
+bool switch1 = 0;   //Left Button
+bool switch2 = 0;   //Right Button
 bool dstat = 1;     //Display Status
-bool dswitch = 0;   //Display Switch
-bool dchange = 0;   //Display Last State
-bool rswitch = 0;   //Reset Switch
-bool rchange = 0;   //Reset Last State
-bool sswitch = 0;   //Relay Trigger
-bool schange = 0;   //Relay Last State
 
-int pageCount = 4;  //Page Count
-int dpage = 0;          //Display Page Memory
 
-int screenrefresh = 1000;  //Screen Refresh Time
+int screenrefresh = 2000;  //Screen Refresh Time
+
+int dmemory [2] = {};
 
 void setup() {
   Serial.begin(9600);
-  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)
+  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
   display.clearDisplay();
 }
 
 void loop() {
-  dswitch = digitalRead(BTN);
-  rswitch = digitalRead(RST);
+  switch1 = digitalRead(BTN1);
+  switch2 = digitalRead(BTN2);
 
   disp();
-  
+  //dmemory[0] = 2; dmemory[1] = 1;
+
   delay(100);
 }
 
 void disp() {
-  if (millis() - disprMillis > screenrefresh || dchange == 1) {
+  if (millis() - disprMillis > screenrefresh) {
+    disprMillis = millis();
+    display.clearDisplay();
     disptop();
-
+    dispmain();
+    display.display();
+    test();
   }
 }
 
+void dispmain() {
+  switch (dmemory[0]) {
+    case 0:
+      disp0();
+      break;
+    case 1:
+      disp1();
+      break;
+    case 2:
+      disp2();
+      break;
+    case 3:
+      disp3();
+      break;
+    case 4:
+      disp4();
+      break;
+  }
+}
 
-void dispo() {
-  if (millis() - disprMillis > screenrefresh) {
-    disprMillis = millis();
-    if (dstat == 1){
-      display.clearDisplay();
+void disptop () {
+  if (dmemory [0] == 0) {
+    //add power icon here
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(12, 0);
+    display.print("Power Meter");
+    dispwifi();
+  } else if (dmemory [0] == 1) {
+    //add settings icon here
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(12, 0);
+    display.print("Settings");
+    dispwifi();
+  } else if (dmemory [0] == 2) {
+    //add settings icon here
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(12, 0);
+    display.print("WiFi Settings");
+    dispwifi();
+  } else if (dmemory [0] == 3) {
+    //add settings icon here
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(12, 0);
+    display.print("Daily Usage Limit");
+    dispwifi();
+  } else if (dmemory [0] == 4) {
+    //add settings icon here
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(12, 0);
+    display.print("Usage Reset");
+    dispwifi();
+  }
+}
 
-      disptop();
-      
-      if(meter == 0){
-        display.setTextSize(2);
-        display.setCursor(38,16); display.println("Error");
-        display.setCursor(27,32); display.println("Reading");
-        display.setTextSize(1); display.setCursor(22,56); display.print("No Power Supply");
-        display.display();
+void disp0() {
+  switch (dmemory[1]) {
+    case 0:
+      display.setTextSize(1); display.setTextColor(SSD1306_WHITE); display.setCursor(0, 16);
+      display.print("Voltage:   "); display.print(voltage,2); display.println(" V");
+      display.print("Current:   "); display.print(current,3); display.println(" A");
+      display.print("Power:     "); display.print(power,2); display.println(" W");
+      display.print("Usage:     "); display.print(energy,3); display.println(" kWh");
+      display.print("Freq.:     "); display.print(frequency,1); display.println(" Hz");
+      display.print("PF:        "); display.print(pf,2);
+      break;
+    case 1:
+      display.setTextSize(2); display.setTextColor(SSD1306_WHITE);
+      display.setCursor(0, 16); display.print(voltage,1); display.println(" V");
+      display.setCursor(0, 40); display.print(current,3); display.println(" A");
+      break;
+    case 2:
+      display.setTextSize(2); display.setTextColor(SSD1306_WHITE);
+      display.setCursor(0, 16); display.print(power,1); display.println(" W");
+      display.setCursor(0, 40); display.print(energy,3); display.println(" kWh");
+      break;
+    case 3: //Last Usage
+      display.setTextSize(1); display.setTextColor(SSD1306_WHITE); display.setCursor(0, 16);
+      display.println("Energy Consumption");
+      display.setCursor(0, 30);
+      display.print("1hr:     "); display.print(hourly,3); display.println(" kwh");
+      display.print("24hrs:   "); display.print(daily,3); display.println(" kWh");
+      display.setCursor(0, 56);
+      display.print("Total:   "); display.print(energy,3); display.println(" kWh");
+      break;
+    case 4: //Daily Limit
+      display.setTextSize(1); display.setTextColor(SSD1306_WHITE); display.setCursor(0, 16);
+      display.println("Usage Limit");
+      display.setCursor(0, 30);
+      display.print("Limit:   "); display.print(hourly,3); display.println(" kwh");
+      display.print("Usage:   "); display.print(daily,3); display.println(" kWh");
+      display.setCursor(0, 56);
+      display.print("Total:   "); display.print(energy,3); display.println(" kWh");
+      break;
+    case 8: //Warning Page
+      //disp08();
+      break;
+    case 9: //No Power Supply
+      display.setTextSize(2); display.setTextColor(SSD1306_WHITE);
+      display.setCursor(34, 16); display.print("Error");
+      display.setCursor(22, 32); display.print("Reading");
+      display.setTextSize(1);
+      display.setCursor(18, 56); display.print("No Power Supply");
+      break;
+    }
+}
 
-        dchange = 0;
-      } else {
-        if (dchange == 1) {
-          dchange = 0;
-          if(dpage == pageCount){
-            dpage = 1;
-          } else {
-            ++dpage;
-          }
-        }
-        switch (dpage) {
-          case 1:
-            disp3();
-            break;
-          case 2:
-            disp1();
-            break;
-          case 3:
-            disp2();
-            break;
-          case 4:
-            disp4();
-            break;
-        }
-      }
-    } else {
-      display.clearDisplay();
-      display.setCursor(0,0);
-      display.print(" ");
-      display.display();
+void disp1() {
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(12, 0); display.print("Settings");
+  display.setCursor(12, 14); display.print("WiFi Settings");
+  display.setCursor(12, 23); display.print("Daily Usage Limit");
+  display.setCursor(12, 32); display.print("Usage Reset");
+  display.setCursor(12, 41); display.print("Back");
+  dispbottom();
+  dispcursor();
+}
+
+void disp2() {
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 16); display.print("Saved SSID:");
+  display.setCursor(0, 26); display.print("SLT_FIBRE");
+  display.setCursor(22, 40); display.print("Back");
+  display.setCursor(74, 40); display.print("Reset");
+  dispbottom();
+  dispcursor();
+}
+
+void disp3() {
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 16); display.print("Current:");
+  display.setCursor(0, 24); display.print("New:");
+  display.setCursor(8, 40); display.print("Back");
+  display.setCursor(42, 40); display.print("Set");
+  display.setCursor(72, 40); display.print("(+)");
+  display.setCursor(102, 40); display.print("(-)");
+  dispbottom();
+  dispcursor();
+}
+
+void disp4() {
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 12); display.print("   Following action    cannot be undo, Do  you want to proceed");
+  display.setCursor(22, 44); display.print("Back");
+  display.setCursor(70, 44); display.print("Proceed");
+  dispbottom();
+  dispcursor();
+}
+
+void dispbottom() {
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(20, 56); display.print("Move");
+  display.setCursor(72, 56); display.print("Select");
+}
+
+void dispcursor () {
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  if (dmemory[0] == 1) {
+    switch (dmemory[1]) {
+      case 0:
+        display.setCursor(2, 14);display.print(">");
+        break;
+      case 1:
+        display.setCursor(2, 23);display.print(">");
+        break;
+      case 2:
+        display.setCursor(2, 32);display.print(">");
+        break;
+      case 3:
+        display.setCursor(2, 41);display.print(">");
+        break;
+    }
+  } else if (dmemory[0] == 2) {
+    switch (dmemory[1]) {
+      case 0:
+        display.setCursor(13, 40);display.print(">");
+        break;
+      case 1:
+        display.setCursor(66, 40);display.print(">");
+        break;
+    }
+  } else if (dmemory[0] == 3) {
+    switch (dmemory[1]) {
+      case 0:
+        display.setCursor(1, 40);display.print(">");
+        break;
+      case 1:
+        display.setCursor(35, 40);display.print(">");
+        break;
+      case 2:
+        display.setCursor(66, 40);display.print(">");
+        break;
+      case 3:
+        display.setCursor(96, 40);display.print(">");
+        break;
+    }
+  } else if (dmemory[0] == 4) {
+      switch (dmemory[1]) {
+      case 0:
+        display.setCursor(13, 44);display.print(">");
+        break;
+      case 1:
+        display.setCursor(62, 44);display.print(">");
+        break;
     }
   }
 }
 
-void disp1 () {
-  display.println("");
-  display.setTextSize(2);
-  display.print(voltage,1);
-  display.println(" V");
-
-  display.setTextSize(1);
-  display.println("");
-
-  display.setTextSize(2);
-  display.print(current,3);
-  display.println(" A");
-  display.display();
+void dispwifi() {
+  //display.drawBitmap(120,0,cross,8,8,WHITE);
 }
 
-void disp2 () {
-  display.println("");
-  display.setTextSize(2);
-  display.print(power,1);
-  display.println(" W");
-
-  display.setTextSize(1);
-  display.println("");
-
-  display.setTextSize(2);
-  display.print(energy,3);
-  display.println(" kWh");
-  display.display();
-}
-
-void disp3 () {
-  display.setTextSize(1);
-  display.println("");
-  display.print("Voltage:   "); display.print(voltage,2); display.println(" V");
-  display.print("Current:   "); display.print(current,3); display.println(" A");
-  display.print("Power:     "); display.print(power,2); display.println(" W");
-  display.print("Usage:     "); display.print(energy,3); display.println(" kWh");
-  display.print("Freq.:     "); display.print(frequency,1); display.println(" Hz");
-  display.print("PF:        "); display.print(pf,2);
-  display.display();
-}
-
-void disp4 () {
-  display.setTextSize(1);
-  display.println();
-  display.println("Energy Consumption");
-  display.println();
-
-  display.print("Hourly:  ");
-  display.print(hourly,3);
-  display.println(" kwh");
-
-  display.print("Daily:   ");
-  display.print(daily,3);
-  display.println(" kWh");
-
-  display.println();
-  display.print("Total:   ");
-  display.print(energy,3);
-  display.println(" kWh");
-  display.display();
-}
-
-void disptop () {
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0,0);
-  display.print("Power Meter");
-  display.drawBitmap(120,0,cross,8,8,WHITE);
-  display.println();
-  display.display();
+void test() {
+  if (dmemory[0] == 0) {
+    switch (dmemory[1]) {
+      case 0:
+        dmemory[1] = 1;
+        break;
+      case 1:
+        dmemory[1] = 2;
+        break;
+      case 2:
+        dmemory[1] = 3;
+        break;
+      case 3:
+        dmemory[1] = 4;
+        break;
+      case 4:
+        dmemory[1] = 9;
+        break;
+      case 9:
+        dmemory[0] = 1;
+        dmemory[1] = 0;
+        break;
+    }
+  } else if (dmemory[0] == 1) {
+    switch (dmemory[1]) {
+      case 0:
+        dmemory[1] = 1;
+        break;
+      case 1:
+        dmemory[1] = 2;
+        break;
+      case 2:
+        dmemory[1] = 3;
+        break;
+      case 3:
+        dmemory[0] = 2;
+        dmemory[1] = 0;
+        break;
+    }
+  } else if (dmemory[0] == 2) {
+    switch (dmemory[1]) {
+      case 0:
+        dmemory[1] = 1;
+        break;
+      case 1:
+        dmemory[0] = 3;
+        dmemory[1] = 0;
+        break;
+    }
+  } else if (dmemory[0] == 3) {
+    switch (dmemory[1]) {
+      case 0:
+        dmemory[1] = 1;
+        break;
+      case 1:
+        dmemory[1] = 2;
+        break;
+      case 2:
+        dmemory[1] = 3;
+        break;
+      case 3:
+        dmemory[0] = 4;
+        dmemory[1] = 0;
+        break;
+    }
+  } else if (dmemory[0] == 4) {
+    switch (dmemory[1]) {
+      case 0:
+        dmemory[1] = 1;
+        break;
+      case 1:
+        dmemory[0] = 0;
+        dmemory[1] = 0;
+        break;
+    }
+  }
 }
